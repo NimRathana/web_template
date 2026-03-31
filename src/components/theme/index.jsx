9c64c0532bf9930d6c13608fa870d17f087b7376
Nim Rathana <126838139+NimRathana@users.noreply.github.com>
2026-03-31 12:31:05 +0700

    commit

--- src/components/theme/index.jsx
+++ src/components/theme/index.jsx
@@ -1,85 +1,41 @@
 'use client'
 
-// React Imports
 import { useMemo } from 'react'
-
-// MUI Imports
-import { deepmerge } from '@mui/utils'
-import {
-  ThemeProvider,
-  extendTheme,
-  lighten,
-  darken
-} from '@mui/material/styles'
+import { ThemeProvider, extendTheme, lighten, darken } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
-
-// Emotion
-import { CacheProvider } from '@emotion/react'
-import createCache from '@emotion/cache'
-
-// Component Imports
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter'
 import ModeChanger from './ModeChanger'
-
-// Config Imports
+import { useSettings } from '@core/hooks/useSettings'
+import defaultCoreTheme from '@core/theme'
 import primaryColorConfig from '@configs/primaryColorConfig'
 
-// Hook Imports
-import { useSettings } from '@core/hooks/useSettings'
-
-// Core Theme
-import defaultCoreTheme from '@core/theme'
-
-// Emotion cache
-const clientSideEmotionCache = createCache({ key: 'css', prepend: true })
-
-const AppThemeProvider = ({ children, direction }) => {
+const CustomThemeProvider = ({ children, direction }) => {
   const { settings } = useSettings()
 
   const theme = useMemo(() => {
-    const newColorScheme = {
-      colorSchemes: {
-        light: {
-          palette: {
-            primary: {
-              main: primaryColorConfig[0].main,
-              light: lighten(primaryColorConfig[0].main, 0.2),
-              dark: darken(primaryColorConfig[0].main, 0.1)
-            }
-          }
-        },
-        dark: {
-          palette: {
-            primary: {
-              main: primaryColorConfig[0].main,
-              light: lighten(primaryColorConfig[0].main, 0.2),
-              dark: darken(primaryColorConfig[0].main, 0.1)
-            }
-          }
+    const colorScheme = {
+      palette: {
+        primary: {
+          main: primaryColorConfig[0].main,
+          light: lighten(primaryColorConfig[0].main, 0.2),
+          dark: darken(primaryColorConfig[0].main, 0.1)
         }
       }
     }
 
-    const coreTheme = deepmerge(
-      defaultCoreTheme(settings.mode || 'light', direction),
-      newColorScheme
-    )
-
-    return extendTheme({
-    ...coreTheme,
-    cssVarPrefix: 'mui',
-    colorSchemeSelector: 'class'
-  })
+    const coreTheme = defaultCoreTheme(settings.mode || 'light', direction)
+    return extendTheme({ ...coreTheme, ...colorScheme })
   }, [settings.mode, direction])
 
   return (
-    <CacheProvider value={clientSideEmotionCache}>
+    <AppRouterCacheProvider options={{ prepend: true }}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <ModeChanger />
         {children}
       </ThemeProvider>
-    </CacheProvider>
+    </AppRouterCacheProvider>
   )
 }
 
-export default AppThemeProvider
+export default CustomThemeProvider
