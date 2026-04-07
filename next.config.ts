7dc6ce96902b2b4ee205ba2b52fff3224846face
Nim Rathana <126838139+NimRathana@users.noreply.github.com>
2026-04-07 17:50:02 +0700

    commit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: [process.env.IP || 'localhost'],
   basePath: process.env.BASEPATH || '',
+  reactStrictMode: true,
 };
 
 export default nextConfig;
